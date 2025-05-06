2d708d90eb275cb81087f4d7ce3a226f18c84be2
Breno <brenomelogabriel2019@gmail.com>
2025-05-05 21:37:36 -0300

    a

--- generate.py
+++ generate.py
@@ -193,9 +193,9 @@ if True:
     # o_model = XLMRobertaForMaskedLM.from_pretrained(config["load_bert"], return_dict=True).to(device)
     # tokenizer = XLMRobertaTokenizer.from_pretrained(config["load_bert"], do_lower_case=False)
 
-    entity_model = AutoModelForMaskedLM.from_pretrained('thegoodfellas/tgf-xlm-roberta-base-pt-br', return_dict=True).to(device)
-    o_model = AutoModelForMaskedLM.from_pretrained('thegoodfellas/tgf-xlm-roberta-base-pt-br', return_dict=True).to(device)
-    tokenizer = AutoTokenizer.from_pretrained('thegoodfellas/tgf-xlm-roberta-base-pt-br', do_lower_case=False)
+    entity_model = AutoModelForMaskedLM.from_pretrained('microsoft/mdeberta-v3-base', return_dict=True).to(device)
+    o_model = AutoModelForMaskedLM.from_pretrained('microsoft/mdeberta-v3-base', return_dict=True).to(device)
+    tokenizer = AutoTokenizer.from_pretrained('microsoft/mdeberta-v3-base', do_lower_case=False)
 
     
     # Add entity labels as special tokens
